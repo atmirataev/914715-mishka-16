60c41c3fa24b2edebdf6e99211296068ffdd5342
Atmir <atmirataev@gmail.com>
2019-04-24 23:24:32 +0300

    Исправляет недочеты по д/з №5

---
+++ source/js/main.js
@@ -0,0 +1,26 @@
+var siteMenu = document.querySelector('.site-menu');
+var userMenu = document.querySelector('.user-menu');
+var navToggle = document.querySelector('.page-header__toggle');
+
+// siteMenu.classList.add('site-menu--closed');
+// userMenu.classList.add('user-menu--closed');
+navToggle.classList.add('page-header__toggle--closed')
+
+navToggle.addEventListener('click', function () {
+    if (siteMenu.classList.contains('site-menu--closed') || userMenu.classList.contains('user-menu--closed')) {
+      siteMenu.classList.remove('site-menu--closed');
+      userMenu.classList.remove('user-menu--closed');
+    } else {
+      siteMenu.classList.add('site-menu--closed');
+      userMenu.classList.add('user-menu--closed');
+
+    }
+});
+
+navToggle.addEventListener('click', function () {
+    if (navToggle.classList.contains('page-header__toggle--closed')) {
+        navToggle.classList.remove('page-header__toggle--closed');
+    } else {
+        navToggle.classList.add('page-header__toggle--closed');
+    }
+});
