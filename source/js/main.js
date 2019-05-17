d15efb52c173eb5025308806c02794657417f61a
Atmir <atmirataev@gmail.com>
2019-05-17 20:49:20 +0300

    Готово все кроме JS

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,13 @@
 var siteMenu = document.querySelector('.site-menu');
 var userMenu = document.querySelector('.user-menu');
 var navToggle = document.querySelector('.page-header__toggle');
+var orderBtn = document.querySelectorAll('.order__button');
+var userCart = document.querySelector('.modal-cart');
 
-navToggle.classList.add('page-header__toggle--closed')
+navToggle.classList.add('page-header__toggle--closed');
+siteMenu.classList.add('site-menu--closed');
+userMenu.classList.add('user-menu--closed');
+
 
 navToggle.addEventListener('click', function () {
     if (siteMenu.classList.contains('site-menu--closed') || userMenu.classList.contains('user-menu--closed')) {
@@ -11,7 +16,6 @@ navToggle.addEventListener('click', function () {
     } else {
       siteMenu.classList.add('site-menu--closed');
       userMenu.classList.add('user-menu--closed');
-
     }
 });
 
@@ -20,5 +24,19 @@ navToggle.addEventListener('click', function () {
         navToggle.classList.remove('page-header__toggle--closed');
     } else {
         navToggle.classList.add('page-header__toggle--closed');
+    }
+});
+
+orderBtn.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    userCart.classList.add('modal__show')
+});
+
+document.addEventListener('keydown', function(evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (userCart.classList.contains('modal__show')) {
+          userCart.classList.remove('modal__show')
+        }
     }
 });
